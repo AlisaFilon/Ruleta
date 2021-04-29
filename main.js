80f4b9d0a77cec12a558aa09a097082688460bd3
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-04-29 14:11:07 +0200

    nada de nada

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+const button=document.getElementById("generate")
+
+function generateName() {
+    function consol() {
+
+    }
+
+    console.log(consol);
+}
+
+button.addEventListener("click", generateName)
+
