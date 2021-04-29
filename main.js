979c7b396f28f254b9998f09ab35abd2641fa517
rossyrom84 <rosangely.romero@gmail.com>
2021-04-29 15:20:17 +0200

    vuelta

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-let btnRandom = document.querySelector('button');
-let result =document.querySelector('h1');
+const button=document.getElementById("generate")
 
-let coders = ['Alisa', 'Angie', 'Gaby', 'Rosangely', 'Yeraldine', 'Eliana', 'Inga', 'DanielaR', 'MariaLaura', 'Taiza', 'Nicole', 'AnaLissette', 'AnaGarcía', 'DanielaT', 'Marta', 'Amanda', 'Yasmin', 'Sònia', 'Leo', 'Pepi', 'Claudia', 'Jolga', 'Flo', 'Ranju'];
+function generateName() {
+    function consol {
 
-function getRandomNumber(min, max) {
-    let step1 = max - min + 1;
-    let step2 = Math.random() * step1;
-    let result = Math.floor(step2) + min;
+    }
 
-    return result;
+    console.log(consol);
 }
 
-btnRandom.addEventListener('click', () => {
-    let index = getRandomNumber(0, coders.length-1);
-    result.innerText = coders[index];
-});
+button.addEventListener("click", generateName)
