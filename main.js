c9ecff7655f9cdfcec15d2d58c18b64a6890ea7d
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-04-29 12:48:26 +0200

    borradojs

--- main.js
+++ main.js
@@ -1,15 +1,1 @@
 
-const firstNames = ["Alisa", "Angie", "Gaby", "Rosangely", "Yeraldine", "Eliana", "Inga", "DanielaR", "MariaLaura", "Taiza", "Nicole", "AnaLissette", "AnaGarcía", "DanielaT", "Marta", "Amanda", "Yasmis", "Sònia", "Leo", "Pepi", "Claudia", "Jolga", "Flo", "Ranju"];
-
-const getRandomNumber = (max) => Math.floor(Math.random() * max);
-
-const getRandomName = () =>
-    `${firstNames[getRandomNumber(firstNames.length)]}`;
-
-const setRandomName = () => {
-    document.getElementById('random-name').innerText = getRandomName();
-}
-document.getElementById('generate')
-    .addEventListener('click', setRandomName);
-
-setRandomName();
