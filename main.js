07c97ca6272f83cdd09250823b54ef2a2e82abe7
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-05-03 09:33:19 +0200

    agregado splice

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const coders = ['Alisa', 'Angie', 'Gaby', 'Rosangely', 'Yeraldine', 'Eliana', 'Inga', 'DanielaR', 'MariaLaura', 'Taiza',
-    'Nicole', 'AnaLissette', 'AnaGarcía', 'DanielaT', 'Marta', 'Amanda', 'Yasmis', 'Sònia', 'Leo', 'Pepi', 'Claudia',
+    'Nicole', 'AnaLissette', 'AnaGarcía', 'DanielaT', 'Marta', 'Amanda', 'Yasmin', 'Sònia', 'Leo', 'Pepi', 'Claudia',
     'Jolga', 'Flo', 'Ranju'];
 
 function randomCoder() {
-    let elegida = coders[Math.floor(Math.random() * coders.length)];
-    document.getElementById("select").innerHTML = elegida;
+    let elegida = Math.floor(Math.random() * coders.length);
+    const norepetir = coders [ randomCoder];
+    let eliminada = coders.splice(elegida,1);
+    document.getElementById("select").innerHTML = eliminada;
 }
 
 let button = document.getElementById("generate");
